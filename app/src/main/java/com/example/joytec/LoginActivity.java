546a59955a3858f27ee1d9b6964c0f9715e6b534
LuisCarlosDelValle-TI <20233l001018@utcv.edu.mx>
2025-06-27 16:06:34 -0600

    app joytec

---
+++ app/src/main/java/com/example/joytec/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.example.joytec;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main); // Asegúrate de tener este layout
+    }
+}
