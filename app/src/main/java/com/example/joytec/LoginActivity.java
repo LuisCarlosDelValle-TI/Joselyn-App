535a79e5c304320a7a4db07b0910406fdc4bcb62
LuisCarlosDelValle <luisdelvalledelvllegmail.com>
2025-07-03 12:25:00 -0600

    subiendo app a Git

--- app/src/main/java/com/example/joytec/LoginActivity.java
+++ app/src/main/java/com/example/joytec/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.example.joytec;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_main); // Asegúrate de tener este layout
+        setContentView(R.layout.login_main);
+
+        Button buttonRegister = findViewById(R.id.buttonRegister);
+        buttonRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, RegistroActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/joytec/RegistroActivity.java
+++ app/src/main/java/com/example/joytec/RegistroActivity.java
@@ -1,4 +1,13 @@
+// app/src/main/java/com/example/joytec/RegistroActivity.java
 package com.example.joytec;
 
-public class RegistroActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro_main);
+    }
 }
